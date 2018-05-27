5468ac56ff0f733b58d15b2abb41f8c512ba2d2d
Jordan Beasley <jordan.beasley@smail.astate.edu>
2018-05-27 21:20:54 +0000

    update

--- routes/stories-api.js
+++ routes/stories-api.js
@@ -61,7 +61,7 @@ router.get('/getGenres', function(req, res, next){
     con.connect(function(err){
         if(err) throw err;
       
-        con.query('SELECT * FROM getGenres', function(err, result, fields){
+        con.query('SELECT * FROM GenreView', function(err, result, fields){
             if(err) throw err;
             
             res.json(result);
@@ -77,7 +77,7 @@ router.get('/getHome', function(req, res, next){
         if(err) throw err;
         if(req.query.userId)
         {
-            con.query('CALL gethome (?)', [req.query.userId], function(err, result, fields){
+            con.query('CALL getHome (?)', [req.query.userId], function(err, result, fields){
                 if(err) throw err;
 
                 res.json(result[0]);
@@ -99,7 +99,8 @@ router.get('/getStory', function(req, res, next){
         if(err) throw err;
         if(req.query.storyId)
         {
-            con.query('SELECT * FROM getStory WHERE id = ?', [req.query.storyId], function(err, result, fields){
+            var uid = (req.query.userId != null) ? req.query.userId : -1;
+            con.query('CALL getStory (?, ?)', [uid, req.query.storyId], function(err, result, fields){
                 if(err) throw err;
 
                 res.json(result[0]);
@@ -114,14 +115,97 @@ router.get('/getStory', function(req, res, next){
 
 });
 
+router.get('/getStoriesByAuthor', function(req, res, next){
+    var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
+  
+    con.connect(function(err){
+        if(err) throw err;
+        if(req.query.authorId)
+        {
+            var uid = (req.query.userId != null) ? req.query.userId : -1;
+            con.query('CALL getStoriesByAuthor (?, ?)', [uid, req.query.authorId], function(err, result, fields){
+                if(err) throw err;
+
+                res.json(result[0]);
+
+            });
+        }
+        else
+        {
+            res.json({});
+        }
+    });
+
+});
+
+router.get('/getAuthorShelf', function(req, res, next){
+    var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
+  
+    con.connect(function(err){
+        if(err) throw err;
+        if(req.query.authorId)
+        {
+            var uid = (req.query.userId != null) ? req.query.userId : -1;
+            con.query('CALL getAuthorShelf (?, ?)', [uid, req.query.authorId], function(err, result, fields){
+                if(err) throw err;
+
+                res.json(result[0]);
+
+            });
+        }
+        else
+        {
+            res.json({});
+        }
+    });
+
+});
+
+
+router.get('/searchStory', function(req, res, next){
+    var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
+  
+    con.connect(function(err){
+        if(err) throw err;
+        if(req.query.qry)
+        {
+            var uid = (req.query.userId != null) ? req.query.userId : -1;
+            if(req.query.genreId)
+            {
+                con.query('CALL searchStoryByGenre (?, ?, ?)', [uid, req.query.qry, req.query.genreId], function(err, result, fields){
+                    if(err) throw err;
+    
+                    res.json(result[0]);
+    
+                });
+            }
+            else
+            {
+                con.query('CALL searchStory (?, ?)', [uid, req.query.qry], function(err, result, fields){
+                    if(err) throw err;
+    
+                    res.json(result[0]);
+    
+                });
+            }
+        }
+        else
+        {
+            res.json({});
+        }
+    });
+
+});
+
 router.get('/getProfile', function(req, res, next){
     var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
   
     con.connect(function(err){
         if(err) throw err;
-        if(req.query.userId)
+        if(req.query.authorId)
         {
-            con.query('SELECT * FROM getProfile WHERE id = ?', [req.query.userId], function(err, result, fields){
+            var uid = (req.query.userId != null) ? req.query.userId : -1;
+            con.query('CALL getProfile (?, ?)', [uid, req.query.authorId], function(err, result, fields){
                 if(err) throw err;
 
                 res.json(result[0]);
@@ -143,7 +227,7 @@ router.get('/getAuthor', function(req, res, next){
         if(err) throw err;
         if(req.query.userId)
         {
-            con.query('SELECT * FROM getAuthor WHERE id = ?', [req.query.userId], function(err, result, fields){
+            con.query('SELECT * FROM AuthorView WHERE id = ?', [req.query.userId], function(err, result, fields){
                 if(err) throw err;
 
                 res.json(result[0]);
@@ -157,7 +241,7 @@ router.get('/getAuthor', function(req, res, next){
     });
 });
 
-router.post('/saveStory', function(req, res, next){
+router.get('/saveStory', function(req, res, next){
     var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
   
     con.connect(function(err){
@@ -169,6 +253,68 @@ router.post('/saveStory', function(req, res, next){
 
                 res.json(result[0][0]);
 
+            });
+        }
+        else
+        {
+            res.json({});
+        }
+    });
+});
+
+router.get('/likeStory', function(req, res, next){
+    var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
+  
+    con.connect(function(err){
+        if(err) throw err;
+        if(req.query.userId && req.query.storyId)
+        {
+            con.query('CALL likeStory (?,?)', [req.query.userId, req.query.storyId], function(err, result, fields){
+                if(err) throw err;
+
+                res.json({});
+
+            });
+        }
+        else
+        {
+            res.json({});
+        }
+    });
+});
+
+router.get('/bookmarkStory', function(req, res, next){
+    var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
+  
+    con.connect(function(err){
+        if(err) throw err;
+        if(req.query.userId && req.query.storyId)
+        {
+            con.query('CALL bookmarkStory (?,?)', [req.query.userId, req.query.storyId], function(err, result, fields){
+                if(err) throw err;
+
+                res.json({});
+
+            });
+        }
+        else
+        {
+            res.json({});
+        }
+    });
+});
+
+router.get('/followAuthor', function(req, res, next){
+    var con = mysql.createConnection({host: process.env.SQL_HOST, user: process.env.SQL_USER, password: process.env.SQL_PASS, database: process.env.SQL_DB});
+  
+    con.connect(function(err){
+        if(err) throw err;
+        if(req.query.userId && req.query.authorId && req.query.userId != req.query.authorId)
+        {
+            con.query('CALL followAuthor (?,?)', [req.query.userId, req.query.authorId], function(err, result, fields){
+                if(err) throw err;
+
+                res.json({});
             });
         }
         else
